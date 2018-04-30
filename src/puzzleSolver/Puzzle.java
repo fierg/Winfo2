48da512ea09e8549bd0ef119c7532564ef71bfac
fierg <sven.fiergolla@gmail.com>
2018-04-30 10:43:25 +0200

    added manhattan dist and further references

--- src/puzzleSolver/Puzzle.java
+++ src/puzzleSolver/Puzzle.java
@@ -1,21 +1,36 @@
 package puzzleSolver;
 
+import java.awt.Point;
+
+/**
+ * The Class Puzzle.
+ */
 public class Puzzle {
 
-	//This is the final state we want to reach in order to solve the puzzle
-	private final int[][] SOLUTION = {{1,2,3},{8,0,4},{7,6,5}};
+	/** The Constant SOLUTION. */
+	// This is the final state we want to reach in order to solve the puzzle
+	public final static int[][] SOLUTION = { { 1, 2, 3 }, { 8, 0, 4 }, { 7, 6, 5 } };
 
-	//Saves the current state of the puzzle
-	int [][] puzzle;
-	
-	public Puzzle(int[][] puzzle){
-		this.puzzle = puzzle;		
+	/** The puzzle. */
+	// Saves the current state of the puzzle
+	int[][] puzzle;
+
+	/**
+	 * Instantiates a new puzzle.
+	 *
+	 * @param puzzle the puzzle
+	 */
+	public Puzzle(int[][] puzzle) {
+		this.puzzle = puzzle;
 	}
 
-	//prints the puzzle to console
-	public void printPuzzle(){
-		for(int i = 0; i < this.puzzle.length; i++){
-			for(int j = 0; j < this.puzzle[i].length; j++){
+	/**
+	 * Prints the puzzle.
+	 */
+	// prints the puzzle to console
+	public void printPuzzle() {
+		for (int i = 0; i < this.puzzle.length; i++) {
+			for (int j = 0; j < this.puzzle[i].length; j++) {
 				System.out.print(this.puzzle[i][j] + " ");
 			}
 			System.out.println();
@@ -23,18 +38,23 @@ public class Puzzle {
 		System.out.println("-----");
 	}
 
-	//prints the puzzle to console and adds the given function+value strings
-	public void printPuzzle(String... functionValues){
+	/**
+	 * Prints the puzzle.
+	 *
+	 * @param functionValues the function values
+	 */
+	// prints the puzzle to console and adds the given function+value strings
+	public void printPuzzle(String... functionValues) {
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < this.puzzle.length; i++){
-			for(int j = 0; j < this.puzzle[i].length; j++){
+		for (int i = 0; i < this.puzzle.length; i++) {
+			for (int j = 0; j < this.puzzle[i].length; j++) {
 				sb.append(this.puzzle[i][j]);
 				sb.append(" ");
 			}
 			sb.append("\n");
 		}
 
-		for(String s : functionValues){
+		for (String s : functionValues) {
 			sb.append(s);
 			sb.append(" ");
 		}
@@ -44,41 +64,42 @@ public class Puzzle {
 	}
 
 	/**
-	 * Tries to move a Tile at the specific coordinates
-	 * @param x the x coordinate
-	 * @param y the y coordinate
+	 * Tries to move a Tile at the specific coordinates.
+	 *
+	 * @param x            the x coordinate
+	 * @param y            the y coordinate
 	 * @return returns true, if the tile was moved
 	 */
-	public boolean moveTile(int x, int y){
-		//check if tile is != 0
+	public boolean moveTile(int x, int y) {
+		// check if tile is != 0
 		if (puzzle[y][x] == 0) {
 			return false;
 		}
-		//check for array bounds:
-		if(x < 0 || y < 0 || x > puzzle.length || y > puzzle.length){
+		// check for array bounds:
+		if (x < 0 || y < 0 || x > puzzle.length || y > puzzle.length) {
 			return false;
 		} else {
-			int xl, xr, yt, yb; //Variables for xleft, xright, ytop, ybottom
-			//Check if an index would be out of bounds
-			//If yes, put that index to the tile we want to move because
-			//this won't throw an exception when reading puzzle[x][y]
-			//and won't swap, because the tile itself isn't 0
-			xl = Math.max(x-1, 0);
-			xr = Math.min(x+1, puzzle.length);
-			yt = Math.max(y-1, 0);
-			yb = Math.min(y+1, puzzle.length);
+			int xl, xr, yt, yb; // Variables for xleft, xright, ytop, ybottom
+			// Check if an index would be out of bounds
+			// If yes, put that index to the tile we want to move because
+			// this won't throw an exception when reading puzzle[x][y]
+			// and won't swap, because the tile itself isn't 0
+			xl = Math.max(x - 1, 0);
+			xr = Math.min(x + 1, puzzle.length);
+			yt = Math.max(y - 1, 0);
+			yb = Math.min(y + 1, puzzle.length);
 
-			//check if 0 is the Neighbourtile:
-			if(puzzle[yt][x] == 0){
+			// check if 0 is the Neighbourtile:
+			if (puzzle[yt][x] == 0) {
 				swap(x, y, x, yt);
 				return true;
-			} else if(puzzle[y][xl] == 0){
+			} else if (puzzle[y][xl] == 0) {
 				swap(x, y, xl, y);
 				return true;
-			} else if(puzzle[y][xr] == 0){
+			} else if (puzzle[y][xr] == 0) {
 				swap(x, y, xr, y);
 				return true;
-			} else if(puzzle[yb][x] == 0){
+			} else if (puzzle[yb][x] == 0) {
 				swap(x, y, x, yb);
 				return true;
 			}
@@ -87,11 +108,17 @@ public class Puzzle {
 	}
 
 	/**
-	 * swaps two Tiles. Doesn't check if one of them is 0 or out of bounds, use moveTile() for public usage
-	 * @param x the x coordinate to be swapped
-	 * @param y the y coordinate to be swapped
-	 * @param x2 the x coordinate to swap with
-	 * @param y2 the y coordinate to swap with
+	 * swaps two Tiles. Doesn't check if one of them is 0 or out of bounds, use
+	 * moveTile() for public usage
+	 * 
+	 * @param x
+	 *            the x coordinate to be swapped
+	 * @param y
+	 *            the y coordinate to be swapped
+	 * @param x2
+	 *            the x coordinate to swap with
+	 * @param y2
+	 *            the y coordinate to swap with
 	 */
 	private void swap(int x, int y, int x2, int y2) {
 		int tmp = puzzle[y][x];
@@ -100,14 +127,15 @@ public class Puzzle {
 	}
 
 	/**
-	 * Calculates the number of tiles that are at a wrong position
+	 * Calculates the number of tiles that are at a wrong position.
+	 *
 	 * @return the number of tiles at the wrong position
 	 */
-	public int wrongTiles(){
+	public int wrongTiles() {
 		int count = 0;
-		for(int i = 0; i < this.puzzle.length; i++){
-			for(int j = 0; j < this.puzzle[i].length; j++){
-				if(puzzle[i][j] != SOLUTION[i][j]){
+		for (int i = 0; i < this.puzzle.length; i++) {
+			for (int j = 0; j < this.puzzle[i].length; j++) {
+				if (puzzle[i][j] != SOLUTION[i][j]) {
 					count++;
 				}
 			}
@@ -116,10 +144,50 @@ public class Puzzle {
 	}
 
 	/**
-	 * Checks whether the puzzle is solved
-	 * @return returns true if all tiles are in the correct order (1,2,3,8,0,4,7,6,5)
+	 * Manhattan dist. of any given tile to its solution tile.
+	 *
+	 * @param tile the tile
+	 * @param puzzle the puzzle
+	 * @param solutionTile the solution tile
+	 * @return the int
 	 */
-	public boolean isSolved(){
+	public int manhattanDist(int tile, int[][] puzzle, int solutionTile) {
+		Point p1 = getPoint(tile, puzzle);
+		Point p2 = getPoint(solutionTile, SOLUTION);
+
+		return (int) (Math.abs(p1.getX() - p2.getX()) + Math.abs(p1.getY() - p2.getY()));
+	}
+
+	/**
+	 * Gets the point coordinates of a tile
+	 *
+	 * @param tile the tile
+	 * @param puzzle the puzzle
+	 * @return the point
+	 */
+	public Point getPoint(int tile, int[][] puzzle) {
+		int collumn = 0;
+		int row = 0;
+		for (int[] col : puzzle) {
+			row = 0;
+			for (int entry : col) {
+				if (entry == tile) {
+					return new Point(collumn, row);
+				}
+				row++;
+			}
+			collumn++;
+		}
+		return null;
+	}
+
+	/**
+	 * Checks whether the puzzle is solved.
+	 *
+	 * @return returns true if all tiles are in the correct order
+	 *         (1,2,3,8,0,4,7,6,5)
+	 */
+	public boolean isSolved() {
 		return this.wrongTiles() == 0;
 	}
 
--- src/puzzleSolver/puzzleSolver.java
+++ src/puzzleSolver/puzzleSolver.java
@@ -3,21 +3,31 @@ package puzzleSolver;
 
 import java.util.StringTokenizer;
 
+/**
+ * The Class puzzleSolver.
+ */
 public class puzzleSolver {
 
+	/** The puzzle. */
 	private static Puzzle puzzle;
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
-		//args[0]: Hier soll das Puzzle, mit Komma als Trennzeichen oder direkt als int[][]-Array, Ÿbergeben werden kšnnen. 
+		//args[0]: Hier soll das Puzzle, mit Komma als Trennzeichen oder direkt als int[][]-Array, ï¿½bergeben werden kï¿½nnen. 
 		//			( z.B.: 1,2,3,4,5,6,7,8,0 )
 		//args[1]: Dieser Parameter soll den Suchalgorithmus variieren. 
 		//			(1: greedy, 2: A* mit h(n) = falsch platzierten Kacheln, 3: A*  mit h(n) = Manhattan-Distanzen)
-		//args[2]: †ber einen Debugging-Parameter soll gewŠhlt werden kšnnen, ob eine vollstŠndige Ausgabe der Suchschritte stattfindet. 
+		//args[2]: ï¿½ber einen Debugging-Parameter soll gewï¿½hlt werden kï¿½nnen, ob eine vollstï¿½ndige Ausgabe der Suchschritte stattfindet. 
 		//			(0: nein, 1: ja)
 
 		//chheck args:
 		if(args.length != 3){
 			printUsage();
+			return;
 		}
 		//Tokenize the Puzzle and generate it
 		StringTokenizer toki = new StringTokenizer(args[0], ",");
@@ -48,10 +58,19 @@ public class puzzleSolver {
 
 	}
 
+	/**
+	 * Solve.
+	 *
+	 * @param strategy the strategy
+	 * @param printSteps the print steps
+	 */
 	private static void solve(SolvingStrategy strategy, int printSteps){
 		strategy.solve(puzzle, printSteps);
 	}
 
+	/**
+	 * Prints the usage.
+	 */
 	private static void printUsage() {
 		System.out.println("Usage: \"Das Puzzle durch Komma getrennt (1,2,3,4,...)\" \"Suchalgorithmus (1: greedy, 2: A* wrong tiles, 3: A* manhattan\" \"Ausgabe der Suchschritte 0:nein, 1:ja\"");
 	}
